8b2bc998d495b3d5d371bad1055a4436fb5b7ae1
Bhushan <deshmukhumesh354@gmail.com>
2023-06-25 16:37:43 +0530

    hamburger changes

---
+++ app.js
@@ -0,0 +1,19 @@
+const hamburgerOpenConst = document.querySelector('.hamburgerOpen');
+const hamburgerCloseConst = document.querySelector('.hamburgerClose');
+const menuPageConst = document.querySelector('.menuPage');
+const pageLinkConst = document.querySelectorAll('.pageLink');
+
+
+function open() {
+    menuPageConst.style.display = 'flex';
+    menuPageConst.style.top = '0';
+  }
+  
+  function close() {
+    menuPageConst.style.top = '-100%';
+  }
+  
+  hamburgerOpenConst.addEventListener('click', open);
+  hamburgerCloseConst.addEventListener('click', close);
+  
+
