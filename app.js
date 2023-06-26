8ce7da7ce70163f120bd25d7d38db268ec75e724
Bhushan <deshmukhumesh354@gmail.com>
2023-06-26 15:55:57 +0530

    config linters

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const hamburgerOpenConst = document.querySelector('.hamburgerOpen');
 const hamburgerCloseConst = document.querySelector('.hamburgerClose');
 const menuPageConst = document.querySelector('.menuPage');
-const pageLinkConst = document.querySelectorAll('.pageLink');
-
+// const pageLinkConst = document.querySelectorAll('.pageLink');
 
 function open() {
-    menuPageConst.style.display = 'flex';
-    menuPageConst.style.top = '0';
-  }
-  
-  function close() {
-    menuPageConst.style.top = '-100%';
-  }
-  
-  hamburgerOpenConst.addEventListener('click', open);
-  hamburgerCloseConst.addEventListener('click', close);
+  menuPageConst.style.display = 'flex';
+  menuPageConst.style.top = '0';
+}
+
+function close() {
+  menuPageConst.style.top = '-100%';
+}
+
+hamburgerOpenConst.addEventListener('click', open);
+hamburgerCloseConst.addEventListener('click', close);
