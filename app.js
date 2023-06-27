d639f0628184128d1c3d637ddaf7f2ac16403fad
Bhushan <deshmukhumesh354@gmail.com>
2023-06-27 15:05:19 +0530

    on click the page goes to desired section

--- app.js
+++ app.js
@@ -15,13 +15,10 @@ function closeMenu() {
 function handlePageLinkClick() {
   closeMenu();
   
-  // Get the target section id from the href attribute of the clicked link
   const targetSectionId = this.getAttribute('href');
   
-  // Use the target section id to get the corresponding section element
   const targetSection = document.querySelector(targetSectionId);
   
-  // Scroll to the target section using smooth scrolling behavior
   targetSection.scrollIntoView({ behavior: 'smooth' });
 }
 
