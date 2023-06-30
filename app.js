d8ef1738af2151fb353e7dcd1e7f859396ddde6a
Bhushan <deshmukhumesh354@gmail.com>
2023-06-30 16:25:18 +0530

    popup for desktop

--- app.js
+++ app.js
@@ -34,54 +34,84 @@ pageLinkConst.forEach((item) => {
 const projects = [
   {
     name: 'Multi Post Stories',
-    description: ' A daily selection of privately personalized reads  Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+    description: 'A daily selection of privately personalized reads...',
     image: 'popupImg.png',
-    technologies: ['HTML', 'Bootstrap', 'Ruby On Rails'],
-    liveLink: 'https://examplelinkofpage.com/live',
-    sourceLink: 'https://examplelinkofpage.com/source',
+    technologies: ['HTML', 'Bootstrap', 'Ruby on Rails'],
+    liveLink: 'https://google.com',
+    sourceLink: 'https://google.com',
   },
   {
     name: 'Professional Art Printing Data',
     description: ' A daily selection of privately personalized reads  Lorem Ipsum is simply dummy text of the printing and typesetting industry ',
     image: 'screen.png',
-    technologies: ['HTML', 'Bootstrap', 'Ruby On Rails'],
-    liveLink: 'https://examplelinkofpage.com/live',
-    sourceLink: 'https://examplelinkofpage.com/source',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveLink: 'https://google.com',
+    sourceLink: 'https://google.com',
   },
   {
     name: 'Professional Art Printing Data',
     description: ' A daily selection of privately personalized reads  Lorem Ipsum is simply dummy text of the printing and typesetting industry ',
     image: 'screen.png',
-    technologies: ['HTML', 'Bootstrap', 'Ruby On Rails'],
-    liveLink: 'https://examplelinkofpage.com/live',
-    sourceLink: 'https://examplelinkofpage.com/source',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveLink: 'https://google.com',
+    sourceLink: 'https://google.com',
   },
   {
     name: 'Professional Art Printing Data',
     description: ' A daily selection of privately personalized reads  Lorem Ipsum is simply dummy text of the printing and typesetting industry ',
     image: 'screen.png',
-    technologies: ['HTML', 'Bootstrap', 'Ruby On Rails'],
-    liveLink: 'https://examplelinkofpage.com/live',
-    sourceLink: 'https://examplelinkofpage.com/source',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveLink: 'https://google.com',
+    sourceLink: 'https://google.com',
   },
   {
     name: 'Professional Art Printing Data',
     description: ' A daily selection of privately personalized reads  Lorem Ipsum is simply dummy text of the printing and typesetting industry ',
     image: 'screen.png',
-    technologies: ['HTML', 'Bootstrap', 'Ruby On Rails'],
-    liveLink: 'https://examplelinkofpage.com/live',
-    sourceLink: 'https://examplelinkofpage.com/source',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveLink: 'https://google.com',
+    sourceLink: 'https://google.com',
   },
   {
     name: 'Professional Art Printing Data',
     description: ' A daily selection of privately personalized reads  Lorem Ipsum is simply dummy text of the printing and typesetting industry ',
     image: 'screen.png',
-    technologies: ['HTML', 'Bootstrap', 'Ruby On Rails'],
-    liveLink: 'https://examplelinkofpage.com/live',
-    sourceLink: 'https://examplelinkofpage.com/source',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveLink: 'https://google.com',
+    sourceLink: 'https://google.com',
+  },
+  {
+    name: 'Professional Art Printing Data',
+    description: ' A daily selection of privately personalized reads  Lorem Ipsum is simply dummy text of the printing and typesetting industry ',
+    image: 'screen.png',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveLink: 'https://google.com',
+    sourceLink: 'https://google.com',
   },
 ];
 
+// generatingg six cards dynamically usinh ayyay
+function generateProjectSection() {
+  const projectContainer = document.querySelector('.myProjects');
+
+  projects.forEach((project) => {
+    const projectMarkup = `
+      <div class="project">
+        <h2>${project.name}</h2>
+        
+        <p>${project.description}</p>
+        <ul class="techStacks">
+          ${project.technologies.map((tech) => `<li><button class="stackButton" type="button">${tech}</button></li>`).join('')}
+        </ul>
+        <button class="firstProjectButton" type="button" onClick="openPopup(${projects.indexOf(project) + 1})">See Project</button>
+      </div>
+    `;
+
+    projectContainer.insertAdjacentHTML('beforeend', projectMarkup);
+  });
+}
+
+// popup window generate
 function openPopup(index) {
   const project = projects[index];
 
@@ -93,28 +123,21 @@ function openPopup(index) {
   const popupLiveLink = document.getElementById('popupLiveLink');
   const popupSourceLink = document.getElementById('popupSourceLink');
 
-  const multiPostPage = document.getElementById('multiPostPage');
-
   popupName.textContent = project.name;
-
   popupTechnologies.innerHTML = project.technologies.map((tech) => `<li>${tech}</li>`).join('');
   popupImage.src = project.image;
-
   popupDescription.textContent = project.description;
   popupLiveLink.onclick = () => window.open(project.liveLink);
   popupSourceLink.onclick = () => window.open(project.sourceLink);
 
   popup.classList.add('open-popup');
-  multiPostPage.classList.add('popup-container');
 }
 
 function closePopup() {
   const popup = document.getElementById('popup');
   popup.classList.remove('open-popup');
-
-  const multiPostPage = document.getElementById('multiPostPage');
-  multiPostPage.classList.remove('popup-container');
 }
 
+window.addEventListener('load', generateProjectSection);
 openPopup();
 closePopup();
